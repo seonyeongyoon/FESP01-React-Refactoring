cac46e5f2a80f159d837899d21bb00a606a58502
seonyeongyoon <ssunnyy0204@gmail.com>
2023-11-10 14:29:20 +0900

    .

---
+++ 03.react/react_todo/src/pages/regist/TodoRegist.jsx
@@ -0,0 +1,15 @@
+//import { axios } from 'axios';
+import Header from "../../layout/Header";
+import Footer from "../../layout/Footer";
+import Nav from "../../layout/Nav";
+//import { linkTo } from "../../Router";
+
+const TodoRegist = ():Element => {
+  const page = document.createElement("div");
+  
+  return (
+    <div>TodoRegist</div>
+  )
+}
+
+export default TodoRegist;
